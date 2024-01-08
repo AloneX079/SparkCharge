f49ddbf3e0342ce83c1949e26a2c32ff2fb6a3c7
Riseonelimit <hazardousdark55@gmail.com>
2024-01-08 15:36:56 +0530

    [FIXED] apierror returning html response

--- backend_prod/src/utils/apierror.js
+++ backend_prod/src/utils/apierror.js
@@ -2,10 +2,9 @@ class apierror extends Error {
     constructor(statusCode, message= "Something went wrong", errors= [], stack= "") {
         super(message)
         this.statusCode = statusCode
-        this.data = null
+        this.data = message
         this.message = message
         this.sucess = false
-        this.errors = errors
 
         if(stack) {
             this.stack = stack
